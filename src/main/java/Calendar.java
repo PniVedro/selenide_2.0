64e911613f39ab12d2518a859fb1009289ee1415
Mikhail Dukhno <ratercorp@gmail.com>
2023-08-25 12:51:27 +0300

    update Test

--- src/main/java/Calendar.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate;
-public class Calendar {
-    public String generateDate(int Days, int)
-
-
-        return a;
-    }
-
-}
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,21 +1,33 @@
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.Test;
-
+import com.codeborne.selenide.Condition;
+import org.openqa.selenium.Keys;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 
 public class CardTest {
-    @Test
-    public void positiveTest() {
-        Calendar calendar = new Calendar();
-        open("http://localhost:9999/");
-        SelenideElement form = $("form");
-        $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id=date] input").setValue(calendar.calc());
-
-
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
+
+    @org.junit.jupiter.api.Test
+    void deliveryCardTest () {
+        open("http://localhost:9999");
+
+        $("[data-test-id=city] input").setValue("Москва");
+        String currentDate = generateDate(3, "dd.MM.yyyy");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").sendKeys(currentDate);
+
+        $("[data-test-id=name] input").setValue("Иван Иванов");
+        $("[data-test-id=phone] input").setValue("+79999999999");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
+    }
 }
